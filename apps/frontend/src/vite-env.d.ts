1cea5613b54e9db13bb5bd10a1e1a7bd1ba6763d
Joon Sunn, Foo <97303880+joonsunn@users.noreply.github.com>
2025-07-06 21:53:59 +0800

    feat: setup frontend (#17)

--- apps/frontend/src/vite-env.d.ts
+++ apps/frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_API_URL: string;
+  // add any other environment variables here, e.g.:
+  // readonly VITE_ANOTHER_VAR: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@packages': path.resolve(__dirname, '../../@packages'),
+    },
+  },
   server: {
     port: 4000,
     open: false,
